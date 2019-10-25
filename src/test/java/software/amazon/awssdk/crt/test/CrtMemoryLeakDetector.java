a66c87963b60c4e67b0810335727b7d6bed44802
Alex Weibel <aweibel@amazon.com>
2019-10-25 13:37:45 -0700

    WIP

--- src/test/java/software/amazon/awssdk/crt/test/CrtMemoryLeakDetector.java
+++ src/test/java/software/amazon/awssdk/crt/test/CrtMemoryLeakDetector.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import org.junit.Assert;
 
+import java.lang.management.*;
+import javax.management.*;
+
 
 /**
  * Checks that the CRT doesn't have any major memory leaks. Probably won't detect very small leaks but will likely find
@@ -33,21 +36,45 @@ public class CrtMemoryLeakDetector {
         leakCheck(DEFAULT_NUM_LEAK_TEST_ITERATIONS, DEFAULT_ALLOWED_MEDIAN_MEMORY_BYTE_DELTA, fn);
     }
 
+
+    private static List<Long> getDeltaMeasurements(List<Long> measurementValues) {
+        List<Long> deltas = new ArrayList<>();
+        for (int i = 0; i < measurementValues.size() - 1; i++) {
+            long prev = measurementValues.get(i);
+            long curr = measurementValues.get(i + 1);
+            long delta = (curr - prev);
+            deltas.add(delta);
+        }
+
+        return deltas;
+    }
+
+    private static long getNumThreads() throws Exception {
+        MBeanServerConnection connection = ManagementFactory.getPlatformMBeanServer();
+        ObjectName threadMxBean = new ObjectName(ManagementFactory.THREAD_MXBEAN_NAME);
+        Integer threadCount = (Integer)connection.getAttribute(threadMxBean, "ThreadCount");
+        Integer deamonThreadCount = (Integer)connection.getAttribute(threadMxBean, "DaemonThreadCount");
+        System.out.println("MxBean.ThreadCount: " + threadCount);
+        System.out.println("MxBean.DaemonThreadCount " + deamonThreadCount);
+        System.out.println("Thread.activeCount: " + Thread.activeCount());
+        System.out.println("Thread.getAllStackTraces.size: " + Thread.getAllStackTraces().size());
+        return threadCount;
+    }
+
     public static void leakCheck(int numIterations, int maxLeakage, Callable<Void> fn) throws Exception {
         List<Long> memoryUsedMeasurements = new ArrayList<>();
+        List<Long> threadUsedMeasurements = new ArrayList<>();
 
         for (int i = 0; i < numIterations; i++) {
             fn.call();
             memoryUsedMeasurements.add(getEstimatedMemoryInUse());
+            threadUsedMeasurements.add(getNumThreads());
         }
 
-        List<Long> memUseDeltas = new ArrayList<>();
-        for (int i = 0; i < memoryUsedMeasurements.size() - 1; i++) {
-            long prev = memoryUsedMeasurements.get(i);
-            long curr = memoryUsedMeasurements.get(i + 1);
-            long delta = (curr - prev);
-            memUseDeltas.add(delta);
-        }
+        List<Long> memUseDeltas = getDeltaMeasurements(memoryUsedMeasurements);
+        List<Long> threadUsedDeltas = getDeltaMeasurements(threadUsedMeasurements);
+
+        System.out.println("Thread Use Deltas: " + threadUsedDeltas.toString());
 
         // Sort from smallest to largest
         memUseDeltas.sort(null);
